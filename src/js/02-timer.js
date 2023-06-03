4e5f4a9b739124172b26e87776bf20575481c8c6
ng-christina <118376146+ng-christina@users.noreply.github.com>
2023-06-03 20:01:22 +0300

    ready task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,75 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const startBtn = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let countdownInterval;
+
+startBtn.disabled = true;
+const options = {
+        enableTime: true,
+        time_24hr: true,
+        defaultDate: new Date(),
+        minuteIncrement: 1,
+        onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    if (selectedDate < new Date()) {
+      alert('Please choose a date in the future');
+      return;
+    }
+    startBtn.disabled = false;
+    startBtn.addEventListener('click', () => {
+      startCountdown(selectedDate);
+      startBtn.disabled = true;
+    });
+  },
+}
+const datePicker = flatpickr("#datetime-picker",options);
+
+function startCountdown(endTime) {
+  countdownInterval = setInterval(() => {
+    const msLeft = endTime - new Date();
+    if (msLeft < 0) {
+      clearInterval(countdownInterval);
+      alert('Time is up!');
+      return;
+    }
+    updateTimer(msLeft);
+  }, 1000);
+}
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function updateTimer(ms) {
+  const { days, hours, minutes, seconds } = convertMs(ms);
+  daysEl.textContent = addLeadingZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+}
+

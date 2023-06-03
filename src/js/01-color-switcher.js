03d19a774ddace66b9ccd3494a403f530b57c153
ng-christina <118376146+ng-christina@users.noreply.github.com>
2023-06-03 16:48:57 +0300

    update task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
 
+
+const body = document.body;
+const stop = document.querySelector('[data-stop]');
+const start = document.querySelector('[data-start]');
+let timerId = null
+
+
+start.addEventListener("click", changeColor) 
+
+stop.addEventListener('click', stopChangeColor);
+function changeColor(event) {
+    timerId = setInterval(() => {
+    body.style.backgroundColor= getRandomHexColor()
+        }, 1000
+     )
+     start.disabled = true
+}
+
+function stopChangeColor(event) {
+        clearInterval(timerId);   
+        start.disabled = false;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }

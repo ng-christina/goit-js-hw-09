0d5086e0b346bba8efdae63e5881ac321af12c82
ng-christina <118376146+ng-christina@users.noreply.github.com>
2023-06-08 20:24:07 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -18,7 +18,7 @@ function onFormSubmit(event) {
         //додаємо до поточної затримки крок збільшення затримки
         currentDelay += Number(step.value);
       }
-    e.target.reset();
+    event.target.reset();
 }
 
 function createPromise(position, delay) {
@@ -35,56 +35,3 @@ function createPromise(position, delay) {
     });
   }
 
-
-
-
-
-//   const form = document.querySelector('.form');
-// form.addEventListener('submit', onFormSubmit);
-// // Функція, яка на момент сабміту форми
-// // викликає функцію createPromise(position, delay) стільки разів, скільки ввели в поле amount.
-// // Під час кожного виклику передаємо їй номер промісу(position), що створюється,
-// // і затримку, враховуючи першу затримку(delay), введену користувачем, і крок(step).
-// function onFormSubmit(e) {
-//   e.preventDefault();
-//   //відслідковуємо подію, тому можемо отримати доступ до інпутів через їх name
-//   const {
-//     elements: { amount, delay, step },
-//   } = e.currentTarget;
-//   //поточна затримка, буде різною на кожній ітерації
-//   //приводимо до числа
-//   let currentDelay = Number(delay.value);
-//   //створюємо цикл для створення кожного окремого проміса
-//   for (let i = 1; i <= amount.value; i += 1) {
-//     createPromise(i, currentDelay)
-//       .then(({ position, delay }) => {
-//         Notiflix.Notify.success(
-//           `:white_check_mark: Fulfilled promise ${position} in ${delay}ms`
-//         );
-//       })
-//       .catch(({ position, delay }) => {
-//         Notiflix.Notify.failure(
-//           `:x: Rejected promise ${position} in ${delay}ms`
-//         );
-//       });
-//     //додаємо до поточної затримки крок збільшення затримки
-//     currentDelay += Number(step.value);
-//   }
-//   //чистимо форму
-//   e.target.reset();
-// }
-// // Функція createPromise поверталє один проміс, який виконується або відхиляється через delay часу.
-// // Значенням промісу є об'єкт з властивостями position і delay зі значеннями однойменних параметрів.
-// function createPromise(position, delay) {
-//   return new Promise((resolve, reject) => {
-//     const shouldResolve = Math.random() > 0.3;
-//     //setTimeout для першої затримки delay
-//     setTimeout(() => {
-//       if (shouldResolve) {
-//         resolve({ position, delay });
-//       } else {
-//         reject({ position, delay });
-//       }
-//     }, delay);
-//   });
-// }

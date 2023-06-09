38a05a6f8cb93f1edc5908f84e357b32b2ee6cad
ng-christina <118376146+ng-christina@users.noreply.github.com>
2023-06-09 14:48:25 +0000

    Deploying to gh-pages from @ ng-christina/goit-js-hw-09@e95fb2104c9907f040e537d31dcbb9b306a7db9a 🚀

---
+++ 01-color-switcher.355b76e0.js
@@ -0,0 +1,2 @@
+const t=document.body,e=document.querySelector("[data-stop]"),d=document.querySelector("[data-start]");let a=null;e.disabled=!0,d.addEventListener("click",(function(l){a=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),d.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(t){clearInterval(a),d.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.355b76e0.js.map
